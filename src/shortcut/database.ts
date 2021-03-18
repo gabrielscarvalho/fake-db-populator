8529a5e8e6eb51fd7212a81b1836a9288fae64d6
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2021-03-18 13:15:53 -0300

    add new class to shortcut

--- src/shortcut/database.ts
+++ src/shortcut/database.ts
@@ -3,3 +3,4 @@ export { Database } from '../core/database/database'
 export { DatabaseReservedWords } from '../core/database/reserved-words'
 export { Table } from '../core/database/table'
 export { PostgresDatabase } from '../impl/PostgresDatabase'
+export { iDatabase, iTable, iColumn, iDataRow, iDataRowColumn, iDataRowParsed, iDatabaseReservedWords, iMap, iParser, iQueryBuilder, iValueGenerator } from  '../interfaces';
