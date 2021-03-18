bfe9b09e7421cec244ae8f89e98142c4d54762cc
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2021-03-18 13:16:51 -0300

    add missing exports

--- dist/shortcut/data.js
+++ dist/shortcut/data.js
@@ -9,5 +9,7 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 exports.__esModule = true;
 var data_row_1 = require("../core/data/data-row");
 __createBinding(exports, data_row_1, "DataRow");
+var data_row_parsed_1 = require("../core/data/data-row-parsed");
+__createBinding(exports, data_row_parsed_1, "DataRowParsed");
 var data_row_column_1 = require("../core/data/data-row-column");
 __createBinding(exports, data_row_column_1, "DataRowColumn");
--- src/shortcut/data.ts
+++ src/shortcut/data.ts
@@ -1,3 +1,4 @@
 export { DataRow } from '../core/data/data-row';
+export { DataRowParsed } from '../core/data/data-row-parsed';
 export { DataRowColumn } from '../core/data/data-row-column';
 
